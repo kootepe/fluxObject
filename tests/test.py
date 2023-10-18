da66f5abe8082ed2f2528238440a66370062b2f8
Eero <eeero93@gmail.com>
2023-10-18 10:58:22 +0300

    Add dataframe test

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@ from calc.fluxer import ordinal_timer_test
 from calc.fluxer import measurement_reader
 from calc.fluxer import aux_data_reader
 import configparser
+import numpy as np
 
 inifile = 'tests/test_ini.ini'
 def config_read(inifile):
@@ -23,5 +24,24 @@ def test_ordinal_timer():
 
 def test_aux_data_ini_parser():
     self = " self"
-    assert aux_data_reader.aux_data_ini_parser(self, aux_dict) == ('/data/path/', ',', 4, '%Y%m%d%H%M', [1, 3], ['datetime', 'air_pressure'], {'datetime':'str', 'air_pressure':'float'})
+    assert aux_data_reader.aux_data_ini_parser(self, aux_dict) == ('./tests/', ',', 4, '%Y%m%d%H%M', [1, 3], ['datetime', 'air_pressure'], {'datetime':'str', 'air_pressure':'float'})
 
+def test_measurement_reader():
+    config = configparser.ConfigParser()
+    config.read(inifile)
+    files = [ '210103.DAT' ]
+    mes_dict = dict(config.items('measurement_data'))
+    measurement = measurement_reader(mes_dict, files)
+    measurement_cols = measurement.measurement_df.dtypes.to_dict()
+    datatypes = { 'ordinal_date': np.dtype('int64'),
+                 'ordinal_time': np.dtype('float64'),
+                 'ordinal_datetime': np.dtype('float64'),
+                 'error_code': np.dtype('int64'),
+                 'date': np.dtype('O'),
+                 'time': np.dtype('O'),
+                 'h2o': np.dtype('float64'),
+                 'co2':np.dtype('float64'),
+                 'ch4':np.dtype('float64'),
+                 }
+    assert measurement.measurement_files == ['210103.DAT']
+    assert measurement.measurement_df.dtypes.to_dict() == datatypes
