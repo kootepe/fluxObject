e273692d308584c1fea543707441c9072b07297c
Eero <eeero93@gmail.com>
2023-10-19 17:20:41 +0300

    Add docstring

--- calc/filter.py
+++ calc/filter.py
@@ -1,4 +1,18 @@
 def date_filter(data_to_filter, filter_tuple):
+    """
+    Filters dataframes with two dates provided in a tuple
+
+    args:
+    ---
+    data_to_filter -- pandas.dataframe
+        The data we want to filter
+    filter_tuple -- tuple
+        (YYYY-MM-DD HH:MM:SS, YYYY-MM-DD HH:MM:SS)
+
+    returns:
+    ---
+    Data that is between these timestamps
+    """
     start_time = data_to_filter.index.searchsorted(filter_tuple[0])
     end_time = data_to_filter.index.searchsorted(filter_tuple[1])
     df = data_to_filter.iloc[start_time:end_time]
