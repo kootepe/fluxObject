4bc4c831145952b4d936d188eef4a37f93def05b
Eero <eeero93@gmail.com>
2023-11-05 17:50:29 +0200

    Add datetime format to snowdepth_df

--- tools/snow_height.py
+++ tools/snow_height.py
@@ -1,10 +1,11 @@
 import sys
 import pandas as pd
 
+
 def read_snow_measurement(snow_depth_file):
     if snow_depth_file:
         snowdepth = pd.read_excel(snow_depth_file)
-        snowdepth["datetime"] = pd.to_datetime(snowdepth["datetime"])
+        snowdepth["datetime"] = pd.to_datetime(snowdepth["datetime"], format="%d/%m/%Y")
         snowdepth.set_index("datetime", inplace=True)
     else:
         d = {
