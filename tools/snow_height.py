eeb32f47bff93823f032851414240a571891866d
Eero <eeero93@gmail.com>
2023-11-05 17:51:28 +0200

    Return None if there's no snowdepth file

--- tools/snow_height.py
+++ tools/snow_height.py
@@ -8,17 +8,9 @@ def read_snow_measurement(snow_depth_file):
         snowdepth["datetime"] = pd.to_datetime(snowdepth["datetime"], format="%d/%m/%Y")
         snowdepth.set_index("datetime", inplace=True)
     else:
-        d = {
-            "datetime": ["01-01-2023", "02-01-2023"],
-            "snowdepth1": [0, 0],
-            "snowdepth2": [0, 0],
-        }
-        snowdepth = pd.DataFrame(data=d)
-        snowdepth["datetime"] = pd.to_datetime(
-            snowdepth["datetime"], format="%d-%m-%Y"
-        )
-        snowdepth.set_index("datetime", inplace=True)
-        print(snowdepth)
+        snowdepth = None
+    return snowdepth
+
 
 if __name__ == "__main__":
     snow_depth_file = sys.argv[1]
