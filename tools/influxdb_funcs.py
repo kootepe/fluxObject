a97be287384efeca3ec55062b60f62cadcf6f649
Eero <eeero93@gmail.com>
2023-11-06 13:10:54 +0200

    Formatting changes

--- tools/influxdb_funcs.py
+++ tools/influxdb_funcs.py
@@ -3,6 +3,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 import logging
 from urllib3.exceptions import NewConnectionError
 
+
 def influx_push(df, influxdb_dict, tag_columns):
     """
     Push data to InfluxDB
@@ -16,17 +17,21 @@ def influx_push(df, influxdb_dict, tag_columns):
     ---
 
     """
-    with ifdb.InfluxDBClient(url=influxdb_dict.get('url'),
-                                token=influxdb_dict.get('token'),
-                                org=influxdb_dict.get('organization'),
-                                ) as client:
+    with ifdb.InfluxDBClient(
+        url=influxdb_dict.get("url"),
+        token=influxdb_dict.get("token"),
+        org=influxdb_dict.get("organization"),
+    ) as client:
         write_api = client.write_api(write_options=SYNCHRONOUS)
-        write_api.write(bucket=influxdb_dict.get('bucket'), record=df,
-                        data_frame_measurement_name=influxdb_dict.get('measurement_name'),
-                        data_frame_timestamp_timezone=influxdb_dict.get('timezone'),
-                        data_frame_tag_columns=tag_columns,
-                        debug=True)
-    logging.info('Pushed data to DB')
+        write_api.write(
+            bucket=influxdb_dict.get("bucket"),
+            record=df,
+            data_frame_measurement_name=influxdb_dict.get("measurement_name"),
+            data_frame_timestamp_timezone=influxdb_dict.get("timezone"),
+            data_frame_tag_columns=tag_columns,
+            debug=True,
+        )
+    logging.info("Pushed data to DB")
 
 
 def check_last_db_timestamp(influxdb_dict):
@@ -38,20 +43,23 @@ def check_last_db_timestamp(influxdb_dict):
 
     returns:
     ---
-    Tables -- object with infludbtimestamps 
+    Tables -- object with infludbtimestamps
     """
     # inflxudb query to get the timestamp of the last input
-    query = f'from(bucket: "{influxdb_dict.get("bucket")}")' \
-        '|> range(start: 0, stop: now())' \
-        f'|> filter(fn: (r) => r["_measurement"] == "{influxdb_dict.get("measurement_name")}")' \
-        '|> keep(columns: ["_time"])' \
-        '|> sort(columns: ["_time"], desc: false)' \
+    query = (
+        f'from(bucket: "{influxdb_dict.get("bucket")}")'
+        '|> range(start: 0, stop: now())'
+        f'|> filter(fn: (r) => r["_measurement"] == "{influxdb_dict.get("measurement_name")}")'
+        '|> keep(columns: ["_time"])'
+        '|> sort(columns: ["_time"], desc: false)'
         '|> last(column: "_time")'
+    )
 
-    client = ifdb.InfluxDBClient(url=influxdb_dict.get('url'),
-                            token=influxdb_dict.get('token'),
-                            org=influxdb_dict.get('organization'),
-                            )
+    client = ifdb.InfluxDBClient(
+        url=influxdb_dict.get("url"),
+        token=influxdb_dict.get("token"),
+        org=influxdb_dict.get("organization"),
+    )
     try:
         tables = client.query_api().query(query=query)
     except NewConnectionError:
@@ -59,9 +67,9 @@ def check_last_db_timestamp(influxdb_dict):
         last_ts = None
         return last_ts
     try:
-        last_ts = tables[0].records[0]['_time'].replace(tzinfo=None)
+        last_ts = tables[0].records[0]["_time"].replace(tzinfo=None)
     except IndexError:
         # if there's no timestamp, return None
         last_ts = None
-    return last_ts
 
+    return last_ts
