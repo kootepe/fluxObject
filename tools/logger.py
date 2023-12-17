7f0e1af9b5f82c748ed643bc85e24f79c0ea6415
Eero <eeero93@gmail.com>
2023-12-17 20:37:49 +0200

    Modify docstring

--- tools/logger.py
+++ tools/logger.py
@@ -3,7 +3,10 @@ import logging.config
 
 
 def init_logger(log_level=None):
-    """Custom logger that has .ini name in the logging message"""
+    """
+    Initiates logger from logging.ini and optionally takes logging level
+    from .ini
+    """
 
     logging.config.fileConfig('logging.ini')
     logger = logging.getLogger("defaultLogger")
