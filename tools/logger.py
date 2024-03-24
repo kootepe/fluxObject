09554c2ee1c38e51595b21f6b024211ce13c9db5
Eero <eeero93@gmail.com>
2024-03-24 08:05:51 +0200

    Add custom formatter
    logger timestamp now has dot instead of comma for hundreths sep

--- tools/logger.py
+++ tools/logger.py
@@ -1,6 +1,16 @@
 import logging
 import logging.config
 
+import logging
+
+class CustomFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        # Original formatTime provides time up to seconds
+        original_formatted_time = super().formatTime(record, datefmt="%Y-%m-%d %H:%M:%S")
+        # Add milliseconds with a dot as a separator
+        formatted_time = f"{original_formatted_time}.{int(record.msecs):03d}"
+        return formatted_time
+
 
 def init_logger(log_level=None):
     """
@@ -11,6 +21,8 @@ def init_logger(log_level=None):
     logging.config.fileConfig('logging.ini')
     logger = logging.getLogger("defaultLogger")
     if log_level:
-        logger.setLevel(getattr(logging, log_level))
+        logger.setLevel(getattr(logging, log_level.upper()))
+    for handler in logger.handlers:
+        handler.setFormatter(CustomFormatter(handler.formatter._fmt))
 
     return logger
