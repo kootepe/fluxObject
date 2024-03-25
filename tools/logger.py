cda5a39c7f8a426e6f1e1d69ef6c96d95d79095f
Eero <eeero93@gmail.com>
2024-03-25 19:14:57 +0200

    Formatting

--- tools/logger.py
+++ tools/logger.py
@@ -3,10 +3,13 @@ import logging.config
 
 import logging
 
+
 class CustomFormatter(logging.Formatter):
     def formatTime(self, record, datefmt=None):
         # Original formatTime provides time up to seconds
-        original_formatted_time = super().formatTime(record, datefmt="%Y-%m-%d %H:%M:%S")
+        original_formatted_time = super().formatTime(
+            record, datefmt="%Y-%m-%d %H:%M:%S"
+        )
         # Add milliseconds with a dot as a separator
         formatted_time = f"{original_formatted_time}.{int(record.msecs):03d}"
         return formatted_time
@@ -18,7 +21,7 @@ def init_logger(log_level=None):
     from .ini
     """
 
-    logging.config.fileConfig('logging.ini')
+    logging.config.fileConfig("logging.ini")
     logger = logging.getLogger("defaultLogger")
     if log_level:
         logger.setLevel(getattr(logging, log_level.upper()))
