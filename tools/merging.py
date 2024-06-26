dcf0d32966fcd090d1be02dc8148da40e179d412
Eero <eeero93@gmail.com>
2024-06-26 12:05:38 +0300

    Remove old merge confict leftovers

--- tools/merging.py
+++ tools/merging.py
@@ -60,87 +60,83 @@ def merge_by_dtx(main_df, cfg):
         sys.exit(0)
 
 
-def merge_by_dtx_and_id(main_df, cfg):
-    """
-    This sole use of this function is to merge AC chamber snowdepth
-    measurement into the main dataframe.
-    It's not as clean as I'd like but it works fast and well.
-    """
-    measurement_df = main_df
-    other_df = cfg.get("df")
-    # other_df = aux_df
-    # main_df.to_csv("tests/test_data/snow_main.csv")
-    # other_df.to_csv("tests/test_data/snow_other.csv")
-    dflist = []
-    chamber_num_list = other_df["chamber"].unique().tolist()
-    for num in chamber_num_list:
-        if is_dataframe_sorted_by_datetime_index(
-            measurement_df
-        ) and is_dataframe_sorted_by_datetime_index(other_df):
-            main_df = measurement_df.copy()
-            other_df = other_df.copy()
-            main_mask = main_df["chamber"] == num
-            other_mask = other_df["chamber"] == num
-            main_df = main_df[main_mask]
-            other_df = other_df[other_mask]
-            other_df = other_df.drop(columns=["chamber"])
-            df = pd.merge_asof(
-                main_df,
-                other_df,
-                left_on="datetime",
-                right_on="datetime",
-                tolerance=pd.Timedelta("1000d"),
-                direction="backward",
-                suffixes=("", "_y"),
-            )
-            df.drop(df.filter(regex="_y$").columns, axis=1, inplace=True)
-            df.set_index("datetime", inplace=True)
-            df["snowdepth"] = df["snowdepth"].fillna(0)
-            df["snowdepth"].astype("float")
-            dflist.append(df)
-        else:
-            logger.info("Dataframes are not properly sorted by datetimeindex")
-            sys.exit(0)
-    df = pd.concat(dflist)
-    df.sort_index(inplace=True)
-    df = pd.merge(
-        main_df, aux_df, left_on=id_col, right_on=id_col, suffixes=("", "_y")
-    )
-    df.drop(df.filter(regex="_y$").columns, axis=1, inplace=True)
-    df.set_index("datetime", inplace=True, drop=True)
-    return df
+# def merge_by_dtx_and_id(main_df, cfg):
+#     """
+#     This sole use of this function is to merge AC chamber snowdepth
+#     measurement into the main dataframe.
+#     It's not as clean as I'd like but it works fast and well.
+#     """
+#     measurement_df = main_df
+#     other_df = cfg.get("df")
+#     # other_df = aux_df
+#     # main_df.to_csv("tests/test_data/snow_main.csv")
+#     # other_df.to_csv("tests/test_data/snow_other.csv")
+#     dflist = []
+#     chamber_num_list = other_df["chamber"].unique().tolist()
+#     for num in chamber_num_list:
+#         if is_df_valid(measurement_df) and is_df_valid(other_df):
+#             main_df = measurement_df.copy()
+#             other_df = other_df.copy()
+#             main_mask = main_df["chamber"] == num
+#             other_mask = other_df["chamber"] == num
+#             main_df = main_df[main_mask]
+#             other_df = other_df[other_mask]
+#             other_df = other_df.drop(columns=["chamber"])
+#             df = pd.merge_asof(
+#                 main_df,
+#                 other_df,
+#                 left_on="datetime",
+#                 right_on="datetime",
+#                 tolerance=pd.Timedelta("1000d"),
+#                 direction="backward",
+#                 suffixes=("", "_y"),
+#             )
+#             df.drop(df.filter(regex="_y$").columns, axis=1, inplace=True)
+#             df.set_index("datetime", inplace=True)
+#             df["snowdepth"] = df["snowdepth"].fillna(0)
+#             df["snowdepth"].astype("float")
+#             dflist.append(df)
+#         else:
+#             logger.info("Dataframes are not properly sorted by datetimeindex")
+#             sys.exit(0)
+#     df = pd.concat(dflist)
+#     df.sort_index(inplace=True)
+#     df = pd.merge(
+#         main_df, other_df, left_on=id_col, right_on=id_col, suffixes=("", "_y")
+#     )
+#     df.drop(df.filter(regex="_y$").columns, axis=1, inplace=True)
+#     df.set_index("datetime", inplace=True, drop=True)
+#     return df
 
 
-def merge_by_dtx(main_df, cfg):
-    aux_df = cfg.get("df")
-    name = cfg.get("name")
-    direction = cfg.get("direction")
-    if direction is None:
-        direction = "nearest"
-    tolerance = cfg.get("tolerance")
-    if tolerance is None:
-        tolerance = "30d"
-    main_df["idx_cp"] = main_df.index
-    aux_df[f"idx_cp_{name}"] = aux_df.index
-    if is_df_valid(main_df) and is_df_valid(aux_df):
-        df = pd.merge_asof(
-            main_df,
-            aux_df,
-            left_on="datetime",
-            right_on="datetime",
-            tolerance=pd.Timedelta(tolerance),
-            direction=direction,
-            suffixes=("", "_y"),
-        )
-        df[f"t_dif_{name}"] = (
-            df["idx_cp"] - df[f"idx_cp_{name}"]
-        ).dt.total_seconds()
-        df.drop(df.filter(regex="_y$").columns, axis=1, inplace=True)
-        df.set_index("datetime", inplace=True)
-        return df
-    else:
-        logger.info("Dataframes are not properly sorted by datetimeindex")
-        sys.exit(0)
+# def merge_by_dtx(main_df, cfg):
+#     aux_df = cfg.get("df")
+#     name = cfg.get("name")
+#     direction = cfg.get("direction")
+#     if direction is None:
+#         direction = "nearest"
+#     tolerance = cfg.get("tolerance")
+#     if tolerance is None:
+#         tolerance = "30d"
+#     main_df["idx_cp"] = main_df.index
+#     aux_df[f"idx_cp_{name}"] = aux_df.index
+#     if is_df_valid(main_df) and is_df_valid(aux_df):
+#         df = pd.merge_asof(
+#             main_df,
+#             aux_df,
+#             left_on="datetime",
+#             right_on="datetime",
+#             tolerance=pd.Timedelta(tolerance),
+#             direction=direction,
+#             suffixes=("", "_y"),
+#         )
+#         df[f"t_dif_{name}"] = (df["idx_cp"] - df[f"idx_cp_{name}"]).dt.total_seconds()
+#         df.drop(df.filter(regex="_y$").columns, axis=1, inplace=True)
+#         df.set_index("datetime", inplace=True)
+#         return df
+#     else:
+#         logger.info("Dataframes are not properly sorted by datetimeindex")
+#         sys.exit(0)
 
 
 def merge_by_dtx_and_id(main_df, cfg):
